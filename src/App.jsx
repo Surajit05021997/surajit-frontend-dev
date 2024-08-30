14d4019260bfeaa78ccbf498051989fdcac51a24
Surajit Maity <surajitmaity12345@gmail.com>
2024-08-31 01:34:24 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.scss';
 import './components/AppHeader';
 import AppHeader from './components/AppHeader';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <AppHeader />
+      <main className="container">
+        <Outlet />
+      </main>
     </div>
   )
 }
