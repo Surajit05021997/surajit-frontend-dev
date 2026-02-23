675c607ec4322485b9c0d07d84a2e1f158854b85
Surajit Maity <surajitmaity12345@gmail.com>
2026-02-24 02:24:59 +0530

    added copy for about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -15,21 +15,19 @@ const AboutPage = () => {
           <div className="body">
             <div>
               <div className="para-title">Who I am</div>
-              <p>I’m Eihab (Pronounced “Ee-hab”) a multi-disciplinary front-end engineer and UI/UX designer based in Rabat, Morocco 🇲🇦.</p>
+              <p>I’m Surajit, a dedicated front-end software developer based in Hyderabad, India 🇮🇳.
+                With 7 years of experience in the tech industry, I specialize in building intuitive, high-performance user interfaces and transforming complex business requirements into seamless, user-centric digital experiences.</p>
             </div>
             <div>
               <div className="para-title">What I Do</div>
-              <p>With two years of invaluable experience in my role at Harmony Technology –– a tech company based here in Rabat,
-                I have honed my skills in React.js, Next.js, TailwindCSS, and TypeScript, allowing me to craft seamless and interactive user experiences.
-                During my time at Harmony Technology, I had the privilege of collaborating on projects for esteemed clients such as the Ministry of Health,
-                Ministry of Education, and Ministry of Justice. It was an incredibly rewarding experience to develop applications
-                that directly impact the lives of Moroccan citizens.</p>
+              <p>Currently, I am a developer at RealPage, where I contribute to large-scale data solutions. My latest project involves developing and maintaining a robust system that scrapes and processes data
+                 from over 40,000 real estate websites across the USA on a daily basis. My technical toolkit spans HTML, CSS, and JavaScript, alongside modern front-end frameworks like Vue, Angular, and React.
+                 This versatility allows me to adapt to complex technical challenges and craft scalable, high-impact web applications.</p>
             </div>
             <div>
               <div className="para-title">What I Did</div>
-              <p>Before delving into the realm of front end engineering, I spent five years as a graphic designer.
-                This background has equipped me with a keen eye for aesthetics and a deep understanding of user-centered design principles.
-                It enables me to seamlessly blend functionality and visual appeal in every project I undertake.</p>
+              <p>Before joining RealPage, I spent five years at Capgemini as a Front-End Developer with a strong specialization in Vue.js. During my tenure, I had the privilege of working extensively with ABN AMRO, the third-largest Dutch bank.
+                Collaborating closely with cross-functional teams, my primary focus was crafting clean, intuitive interfaces that delivered secure and efficient digital experiences for the bank's extensive user base.</p>
             </div>
             <div>
               <p>Feel free to reach out via e-mail, or follow me on Twitter.
